f16635190fa492b20f7ad33d254c75e30b15750c
Reo Ito <leo.0210.ito@gmail.com>
2022-02-21 23:36:16 +0900

    exec annotate to models branch: main

--- spec/models/driver_spec.rb
+++ spec/models/driver_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: drivers
+#
+#  id                        :bigint           not null, primary key
+#  deleted_at                :datetime
+#  hourly_pay                :integer          default(0)
+#  minimum_guaranteed_salary :integer          default(0)
+#  name                      :string           not null
+#  uid                       :string
+#  created_at                :datetime         not null
+#  updated_at                :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Driver, type: :model do
--- spec/models/extra_salary_spec.rb
+++ spec/models/extra_salary_spec.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: extra_salaries
+#
+#  id                :bigint           not null, primary key
+#  amount            :integer          default(0)
+#  deleted_at        :datetime
+#  is_base_extra     :boolean          default(FALSE), not null
+#  is_range_extra    :boolean          default(FALSE), not null
+#  job_number        :integer          not null
+#  over_time_minutes :integer          default(0)
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  driver_id         :bigint           not null
+#
+# Indexes
+#
+#  index_extra_salaries_on_driver_id  (driver_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (driver_id => drivers.id)
+#
 require 'rails_helper'
 
 RSpec.describe ExtraSalary, type: :model do

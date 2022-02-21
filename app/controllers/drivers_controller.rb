95ddbfdaab49c9c3fc674a66e18232cd39aa97f1
Reo Ito <leo.0210.ito@gmail.com>
2022-02-22 00:05:22 +0900

    Revert "Create controller to test"
    This reverts commit eeb9bfcb0b1491dd8adec959af9a55fb26cb47ab.

--- app/controllers/drivers_controller.rb
+++
@@ -1,9 +0,0 @@
-module Api
-  module V1
-    class DriversController < ApplicationController
-      def index
-        render json: {status: 'SUCCESS', message: 'There\'s no information you are looking for'}, status: :ok
-      end
-    end
-  end
-end

ac1fe592a3f2b508d52a57224111ba3587b0e4ed
Reo Ito <leo.0210.ito@gmail.com>
2022-03-15 00:12:03 +0900

    Driverモデルのrspec追加

--- spec/models/driver_spec.rb
+++ spec/models/driver_spec.rb
@@ -14,5 +14,33 @@
 require 'rails_helper'
 
 RSpec.describe Driver, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Validations' do
+    let!(:driver) { create :driver }
+
+    context 'presence' do
+      it 'nameがあればvalidであること' do
+        driver.update name: 'test'
+        driver.valid?
+        expect(driver).to be_valid
+      end
+    end
+
+    context '文字数制限' do
+      it 'nameは1文字以上であること' do
+        driver.update name: ''
+        driver.valid?
+        expect(driver.errors[:name]).to include("is too short (minimum is 1 character)")
+      end
+
+      it 'hourly_payとminimum_guaranteed_salaryは6桁以上だとinvalidであること' do
+        driver.update(
+          hourly_pay: 1234567,
+          minimum_guaranteed_salary: 1234567
+        )
+        driver.valid?
+        expect(driver.errors[:hourly_pay]).to include("is too long (maximum is 6 characters)")
+        expect(driver.errors[:minimum_guaranteed_salary]).to include("is too long (maximum is 6 characters)")
+      end
+    end
+  end
 end

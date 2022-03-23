1364b4eebd0f88d855d18597d780221ac61e925b
Reo Ito <leo.0210.ito@gmail.com>
2022-03-23 23:54:34 +0900

    spec/factory配下でannotateが実行されたためコミット

--- spec/factories/fixed_salaries.rb
+++ spec/factories/fixed_salaries.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: fixed_salaries
+#
+#  id             :bigint           not null, primary key
+#  deleted_at     :datetime
+#  total          :integer          not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  work_report_id :bigint           not null
+#
+# Indexes
+#
+#  index_fixed_salaries_on_work_report_id  (work_report_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (work_report_id => work_reports.id)
+#
 FactoryBot.define do
   factory :fixed_salary do
     total { Random.rand(1000..20000) }

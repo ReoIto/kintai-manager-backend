b51944f28303eb0cbf1a7bf47ad2d53beb32015c
Reo Ito <leo.0210.ito@gmail.com>
2022-03-31 01:21:20 +0900

    serviceクラスに記述していたメソッドをインスタンスメソッドにする

--- spec/models/work_report_spec.rb
+++ spec/models/work_report_spec.rb
@@ -31,5 +31,31 @@
 require 'rails_helper'
 
 RSpec.describe WorkReport, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'instance methods' do
+    let!(:work_report){ create :work_report }
+
+    context '#calc_working_minutes' do
+      # 同日に出発/到着が完結するとき
+      example 'When departure and arrival are on the same day' do
+        work_report.update(
+          departure_time: '09:00'.to_time,
+          arrival_time: '12:00'.to_time
+        )
+
+        working_minutes = work_report.calc_working_minutes
+        expect(working_minutes).to eq 180
+      end
+
+      # 到着時刻が日を跨ぐとき
+      example 'When arrival_time is on the next day' do
+        work_report.update(
+          departure_time: '23:00'.to_time,
+          arrival_time: '01:00'.to_time
+        )
+
+        working_minutes = work_report.calc_working_minutes
+        expect(working_minutes).to eq 120
+      end
+    end
+  end
 end

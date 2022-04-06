2a21f4dc22662120b68876327d5696e4732d7e6f
Reo Ito <leo.0210.ito@gmail.com>
2022-04-06 23:30:30 +0900

    rspec リファクタリング

--- spec/models/work_report_spec.rb
+++ spec/models/work_report_spec.rb
@@ -32,29 +32,33 @@ require 'rails_helper'
 
 RSpec.describe WorkReport, type: :model do
   describe 'instance methods' do
-    let!(:work_report){ create :work_report }
+    let!(:work_report) do
+      create :work_report,
+        departure_time: departure_time,
+        arrival_time: arrival_time
+    end
 
-    context '#calc_working_minutes' do
-      # 同日に出発/到着が完結するとき
-      example 'When departure and arrival are on the same day' do
-        work_report.update(
-          departure_time: '09:00'.to_time,
-          arrival_time: '12:00'.to_time
-        )
+    describe '#calc_working_minutes' do
+      subject { work_report.calc_working_minutes }
 
-        working_minutes = work_report.calc_working_minutes
-        expect(working_minutes).to eq 180
+      context '同日に出発/到着が完結する時' do
+        let!(:departure_time) { '09:00'.to_time }
+        let!(:arrival_time) { '12:00'.to_time }
+
+        it do
+          subject
+          is_expected.to eq 180
+        end
       end
 
-      # 到着時刻が日を跨ぐとき
-      example 'When arrival_time is on the next day' do
-        work_report.update(
-          departure_time: '23:00'.to_time,
-          arrival_time: '01:00'.to_time
-        )
+      context '同日に出発/到着が完結する時' do
+        let!(:departure_time) { '23:00'.to_time }
+        let!(:arrival_time) { '01:00'.to_time }
 
-        working_minutes = work_report.calc_working_minutes
-        expect(working_minutes).to eq 120
+        it do
+          subject
+          is_expected.to eq 120
+        end
       end
     end
   end
